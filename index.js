0e50a287863efa1364b03700f28ac9649c75575f
Nico Reed <nr830411@gmail.com>
2020-01-20 17:31:36 -0500

    (created index/JSON package & initial functions. axios, inquirer, html-pdf, fs installed)

---
+++ index.js
@@ -0,0 +1,7 @@
+function initProgram() {
+    const axios = require("axios");
+    const inquirer = require("inquirer");
+    const fs = require('fs');
+    const pdf = require('html-pdf');
+
+initProgram()
